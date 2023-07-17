a8ae8bde0ed3b5357809e523a65daac570d10848
Lukas Panneke <l.panneke@googlemail.com>
2023-07-17 14:25:05 +0200

    Add example net only terminating with cut-off*

--- src/test/java/de/lukaspanneke/masterthesis/Examples.java
+++ src/test/java/de/lukaspanneke/masterthesis/Examples.java
@@ -110,6 +110,47 @@ public class Examples {
 		return new Net(new Marking(Map.of(s0, i)));
 	}
 
+	Net cutoff_star_example() {
+		Variable
+				x = new Variable("x"),
+				y = new Variable("y"),
+				z = new Variable("z");
+		Place
+				a = new Place(1, "a"),
+				b = new Place(2, "b"),
+				c = new Place(3, "c"),
+				d = new Place(4, "d"),
+				p = new Place(5, "p");
+		Transition
+				alpha = new Transition(1, "α"),
+				beta = new Transition(2, "β"),
+				gamma = new Transition(3, "γ"),
+				delta = new Transition(4, "δ"),
+				epsilon = new Transition(5, "ε"),
+				t = new Transition(6, "t", y.eq(x.plus(1)));
+
+		link(a, x, alpha);
+		link(alpha, x, b);
+		link(b, x, beta);
+		link(beta, x, a);
+
+		link(c, x, gamma);
+		link(gamma, x, d);
+		link(d, x, delta);
+		link(delta, x, c);
+
+		link(b, z, epsilon);
+		link(d, z, epsilon);
+		link(p, x, epsilon);
+		link(epsilon, z, b);
+		link(epsilon, z, d);
+		link(epsilon, y, p);
+		link(p, x, t);
+		link(t, y, p);
+
+		return new Net(new Marking(Map.of(a, 0, c, 0, p, 0)));
+	}
+
 	Net failed_attempt_cutoff_star_example() {
 		Variable
 				x = new Variable("x"),
