e17d03ce811174fc57551928b1e6f5da129f0d90
Lukas Panneke <l.panneke@googlemail.com>
2023-07-18 15:18:02 +0200

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ repositories {
 
 dependencies {
     implementation("com.google.guava:guava:31.1-jre")
-    testImplementation(platform("org.junit:junit-bom:5.9.2"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation(files("/home/lukas/.src-programs/cvc5/build/install/share/java/cvc5.jar"))
 }
 
