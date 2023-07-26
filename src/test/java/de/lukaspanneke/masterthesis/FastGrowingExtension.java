4a92b9b6f256f48e62c985dc735aed112dfd6358
Lukas Panneke <l.panneke@googlemail.com>
2023-07-27 00:35:25 +0200

    Add fast-growing expansion example

---
+++ src/test/java/de/lukaspanneke/masterthesis/FastGrowingExtension.java
@@ -0,0 +1,117 @@
+package de.lukaspanneke.masterthesis;
+
+import de.lukaspanneke.masterthesis.components.Place;
+import de.lukaspanneke.masterthesis.components.Transition;
+import de.lukaspanneke.masterthesis.logic.Domain;
+import de.lukaspanneke.masterthesis.logic.Variable;
+import de.lukaspanneke.masterthesis.net.Marking;
+import de.lukaspanneke.masterthesis.net.Net;
+import de.lukaspanneke.masterthesis.unfolding.Unfolding;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+import static de.lukaspanneke.masterthesis.NetConstruction.link;
+import static de.lukaspanneke.masterthesis.NetConstruction.renderAndClip;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FastGrowingExtension {
+
+	Net net(int n, int m) {
+		Domain domain = v -> v.geq(1).and(v.leq(m));
+		Variable x = new Variable("x", domain);
+		Variable y = new Variable("y", domain);
+		Transition t = new Transition(0);
+		Map<Place, Integer> initial = new HashMap<>();
+		int p_idx = 1;
+		for (int t_idx = 1; t_idx <= n; t_idx++) {
+			Transition transition = new Transition(t_idx);
+			Place pre = new Place(p_idx++, "a" + t_idx);
+			initial.put(pre, 1);
+			Place post = new Place(p_idx++, "b" + t_idx);
+			Variable in = new Variable("x" + t_idx);
+			Variable out = new Variable("y" + t_idx);
+			link(pre, x, transition);
+			link(transition, y, post);
+			link(post, in, t);
+			link(t, out, post);
+		}
+		return new Net(new Marking(initial));
+	}
+
+	Net expansion(int n, int m) {
+		List<Set<Place>> allPost = new ArrayList<>(m * n);
+		Map<Place, Integer> initial = new HashMap<>();
+		int t_idx = 1;
+		int p_idx = 1;
+		for (int strand = 1; strand <= n; strand++) {
+			Set<Place> thisPost = new HashSet<>();
+			String preName = "a" + strand + ".1";
+			Place pre = new Place(p_idx++);
+			initial.put(pre, 1);
+			for (int i = 1; i <= m; i++) {
+				Transition transition = new Transition(t_idx++);
+				link(pre, transition);
+				String postName = "b" + transition.index() + "." + i;
+				Place post = new Place(p_idx++);
+				thisPost.add(post);
+				link(transition, post);
+			}
+			allPost.add(thisPost);
+		}
+		for (Place[] inPlaces : new CartesianProduct<>(Place[]::new, allPost)) {
+			for (Place[] outPlaces : new CartesianProduct<>(Place[]::new, allPost)) {
+				int trans_index = t_idx++;
+				String transitionName = "t" + trans_index + Arrays.toString(inPlaces) + Arrays.toString(outPlaces);
+				Transition transition = new Transition(trans_index);
+				for (Place inPlace : inPlaces) {
+					link(inPlace, transition);
+				}
+				for (Place outPlace : outPlaces) {
+					link(transition, outPlace);
+				}
+			}
+		}
+		return new Net(new Marking(initial));
+	}
+
+	@Test
+	void hl_unfolding() {
+		Net net = net(2, 2);
+		Unfolding unfolding = Unfolding.unfold(net);
+		renderAndClip(unfolding);
+	}
+
+	@Test
+	void expansion_unfolding() {
+		Net net = expansion(2, 3);
+		Unfolding unfolding = Unfolding.unfold(net);
+		renderAndClip(unfolding);
+	}
+
+	void ll_net(int n, int m) {
+		Net expansion = expansion(n, m);
+		//renderAndClip(expansion);
+		Net.Nodes nodes = expansion.collectNodes();
+		assertEquals(n + n * m, nodes.places().size());
+		assertEquals(n * m + Math.pow(m, 2 * n), nodes.transitions().size());
+
+		try (Writer out = Files.newBufferedWriter(Path.of("/home/lukas/ma/mole-140428", "expan-" + n + "-" + m + ".ll"))) {
+			expansion.renderLlPep(out);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	void speed(int n, int m) {
+		Net expansion = expansion(n, m);
+		long before = System.currentTimeMillis();
+		Unfolding.unfold(expansion);
+		long after = System.currentTimeMillis();
+		System.out.println(n + "," + m + " took " + (after - before) + " ms");
+	}
+}
