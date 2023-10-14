c7752f6b156aaaa67e75c39b8e09bd7c168ed95b
Lukas Panneke <l.panneke@googlemail.com>
2023-10-14 23:32:11 +0200

    Apply shellcheck suggestions

--- get-cvc5.sh
+++ get-cvc5.sh
@@ -3,27 +3,27 @@
 tag="cvc5-1.0.8"
 
 mkdir -p build
-cd build
+cd build || exit 1
 
 # get cvc5 release
 if [ ! -d cvc5 ]
 then
 	echo "Cloning $tag"
 	git clone --depth 1 --branch "$tag" https://github.com/cvc5/cvc5.git || exit 1
-	cd cvc5
+	cd cvc5 || exit 1
 else
 	echo "Updating to $tag"
-	cd cvc5
+	cd cvc5 || exit 1
 	git fetch origin tag "$tag"
 	git checkout "$tag" || exit 1
 fi
 
 # prepare build
 ./configure.sh production --static-binary --java-bindings --no-poly --auto-download --prefix=build/install || exit 1
-cd build
+cd build || exit 1 
 
 # build on all cores
-make -j$(nproc) || exit 1
+make -j"$(nproc)" || exit 1
 
 make install || exit 1
 
--- get-z3.sh
+++ get-z3.sh
@@ -3,17 +3,17 @@
 tag="z3-4.12.2"
 
 mkdir -p build
-cd build
+cd build || exit 1
 
 # get z3 release
 if [ ! -d z3 ]
 then
 	echo "Cloning $tag"
 	git clone --branch "$tag" https://github.com/Z3Prover/z3 || exit 1
-	cd z3
+	cd z3 || exit 1
 else
 	echo "Updating to $tag"
-	cd z3
+	cd z3 || exit 1
 	git fetch origin tag "$tag"
 	git checkout "$tag" || exit 1
 fi
@@ -24,10 +24,10 @@ git cherry-pick -n c9d8e646ed505a9cd4c84b2cb31d846b8588109f
 
 # prepare build
 python scripts/mk_make.py --java || exit 1
-cd build
+cd build || exit 1
 
 # build on all cores
-make -j$(nproc) || exit 1
+make -j"$(nproc)" || exit 1
 
 # move back to start directory
 cd ../../..
