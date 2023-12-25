b13296a6510ebf64aafdcd790d7cf24c1b1eec19
Lukas Panneke <l.panneke@googlemail.com>
2023-12-25 23:51:11 +0100

    Update z3

--- get-z3.sh
+++ get-z3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tag="z3-4.12.2"
+tag="z3-4.12.4"
 
 mkdir -p build
 cd build || exit 1
@@ -17,10 +17,6 @@ else
 	git fetch origin tag "$tag"
 	git checkout "$tag" || exit 1
 fi
-
-# Fix build with GCC 13
-# TODO remove this line after updating to a version newer than z3-4.12.2
-git cherry-pick -n c9d8e646ed505a9cd4c84b2cb31d846b8588109f
 
 # prepare build
 python scripts/mk_make.py --java || exit 1
